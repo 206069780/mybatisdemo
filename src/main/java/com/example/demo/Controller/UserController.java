d8d3c1f7633078c9615924f68e33adcbf958083b
com_admin <com_admin@gmail.com>
2021-04-08 14:53:58 +0800

    优化finduser返回的数据

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,15 +1,10 @@
 package com.example.demo.Controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.example.demo.bean.RequestBean;
 import com.example.demo.model.User;
 import com.example.demo.service.iml.UserServiceIml;
 import com.example.demo.utlis.RuturnMessage;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +32,11 @@ public class UserController {
     @Autowired
     RuturnMessage ruturnMessage;
 
+    /**
+     * 保存用户 API
+     * @param request HttpServletRequest
+     * @return 是否保存成功 返回值
+     */
     @ResponseBody
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public Object save(HttpServletRequest request) {
@@ -45,7 +45,7 @@ public class UserController {
         String sex = request.getParameter("sex");
         int age = Integer.parseInt(request.getParameter("age"));
         if (age < 0 || age > 120) {
-            return ruturnMessage.failure("参数错误");
+            return ruturnMessage.failure("年龄参数出错，请检查");
         }
         User user = new User(username, age, sex);
         try {
@@ -53,23 +53,29 @@ public class UserController {
             return "Code:200,message:save ok";
         } catch (Exception e) {
             String errorMessage = String.valueOf(e);
-            return e;
+            return errorMessage;
         }
     }
 
+    /**
+     *  使用id查询用户
+     * @param id 查询的id
+     * @return 返回查询到底的user信息
+     */
     @RequestMapping(value = "finduser", method = RequestMethod.GET)
     @ResponseBody
     public Object findUser(@RequestParam("id") int id) {
         User user = null;
         try {
             user = (User) userServiceIml.getUserById(id);
-            if (user != null) {
+            if (user.getId()==id) {
                 return user;
             }
         } catch (Exception e) {
-            return e.toString();
+            return ruturnMessage.failure("用户不存在");
         }
-        return null;
+        System.out.println(user.getSex());
+        return ruturnMessage.failure("用户不存在");
     }
 
 
